d6de92ea10f5ed7c8c015c20fff876a6d885a90b
sber <mail2bergersi@gmail.com>
2021-12-09 13:44:40 +0100

    Lawine

---
+++ Assets/MyGame/Scripts/Avalanche.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avalanche : MonoBehaviour
+{
+    public GameObject snowball;
+    public GameObject filledHole;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        filledHole.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        filledHole.SetActive(true);
+    }
+}
