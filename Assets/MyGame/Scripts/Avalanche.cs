542e566e8208a886d4bdccf6b5e4a00dfd611848
Kuschlsoke <mail2bergersi@gmail.com>
2022-01-20 12:16:16 +0100

    Texturen und Partikel

--- Assets/MyGame/Scripts/Avalanche.cs
+++ Assets/MyGame/Scripts/Avalanche.cs
@@ -6,11 +6,13 @@ public class Avalanche : MonoBehaviour
 {
     public GameObject snowball;
     public GameObject filledHole;
+    public GameObject lawine;
 
     // Start is called before the first frame update
     void Start()
     {
         filledHole.SetActive(false);
+        lawine.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,5 +24,6 @@ public class Avalanche : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         filledHole.SetActive(true);
+        lawine.SetActive(true);
     }
 }
